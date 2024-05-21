7d4cd19dc5f5f0a9fd7bc479318986a78fba1a85
zhangsimiao <zhangsimiao1992@gmail.com>
2024-05-21 13:14:30 -0700

    testing

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+       String message ="Hello World"+"!!";
+        System.out.println(mess);
+
+    }
+}
